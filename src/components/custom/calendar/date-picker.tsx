3d2d63b135b3077bbb3e0a6b221cacf8fe698a5d
Ricardo Villalta <sidernes22@gmail.com>
2025-04-22 09:50:32 -0600

    refactor: replace Popover and Calendar components with DayPicker for date selection

--- src/components/custom/calendar/date-picker.tsx
+++ src/components/custom/calendar/date-picker.tsx
@@ -1,57 +1,35 @@
 /** @format */
 
-"use client";
+// src/components/custom/calendar/date-picker.tsx
+import React from "react";
+import { format } from "date-fns/format";
+import { es } from "date-fns/locale";
+import { DayPicker } from "react-day-picker";
 
-import * as React from "react";
-import { format } from "date-fns";
-import { es } from "date-fns/locale"; // Importar localización en español
-import { CalendarIcon } from "lucide-react";
+interface DatePickerProps {
+	date: Date | undefined;
+	setDate: (date: Date) => void;
+}
 
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-} from "@/components/ui/popover";
-
-export function DatePicker() {
-	// Estado inicial con la fecha actual
-	const [date, setDate] = React.useState<Date>(new Date());
-
+export function DatePicker({ date, setDate }: DatePickerProps) {
 	return (
-		<Popover>
-			<PopoverTrigger asChild>
-				<Button
-					variant={"outline"}
-					className={cn(
-						"w-[240px] justify-start text-left font-normal",
-						!date && "text-muted-foreground"
-					)}
-				>
-					<CalendarIcon className="mr-2 h-4 w-4" />
-					{/* Mostrar fecha en español */}
-					{date ? (
-						format(date, "PPP", { locale: es })
-					) : (
-						<span>Selecciona una fecha</span>
-					)}
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent className="w-auto p-0" align="start">
-				<Calendar
-					mode="single"
-					selected={date}
-					onSelect={(newDate) => {
-						if (newDate) {
-							setDate(newDate); // Actualiza la fecha seleccionada
-						}
-					}}
-					locale={es}
-					initialFocus
-				/>
-			</PopoverContent>
-		</Popover>
+		<div>
+			<DayPicker
+				mode="single"
+				selected={date}
+				onSelect={(newDate: Date | undefined) => {
+					if (newDate) {
+						setDate(newDate);
+					}
+				}}
+			/>
+			<div>
+				{date ? (
+					format(date, "PPP", { locale: es })
+				) : (
+					<span>Selecciona una fecha</span>
+				)}
+			</div>
+		</div>
 	);
 }
